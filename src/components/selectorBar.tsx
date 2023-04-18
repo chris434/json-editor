79e7311eaa4d9d6520f1a5efecbbee8d8e71ee4c
chris434 <christopherpearce589@gmail.com>
2023-04-18 20:57:05 +0100

    add array and object fields

--- src/components/selectorBar.tsx
+++ src/components/selectorBar.tsx
@@ -1,12 +1,13 @@
 import { useRef } from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import { Select } from "./select";
 import { TooLTipButton } from "./toolTipButton";
-import { TOP_PARENT_TYPES } from "../data/typesData";
+import { TOP_PARENT_TYPES, TYPES } from "../data/typesData";
 import { useJson } from "../providers/jsonProvider";
 
 type selectorBarProps = {
@@ -20,7 +21,14 @@ export function SelectorBar({
 }: selectorBarProps) {
   const previousType = useRef("");
   const { dispatchJson, jsonData } = useJson();
-  const { collapsed, type, childrenIndexes } = jsonData.data[dataIndex];
+  const { collapsed, type, childrenIndexes, parentIndex } = jsonData.data[
+    dataIndex
+  ];
+  const types = selectType === "default" ? TYPES : TOP_PARENT_TYPES;
+
+  const parentType =
+    parentIndex !== null ? jsonData.data[parentIndex].type : null;
+  console.log(parentType);
 
   const getChildren = () => {
     return jsonData.data.filter((itemA, i) => {
@@ -32,6 +40,16 @@ export function SelectorBar({
     });
   };
 
+  const getArrayIndex = () => {
+    if (parentType == "Array") {
+      return jsonData.data[parentIndex].childrenIndexes.indexOf(dataIndex);
+    }
+  };
+
+  const isMultipleType = () => {
+    return type === "Array" || type === "Object";
+  };
+
   return (
     <Box>
       <Box
@@ -40,50 +58,87 @@ export function SelectorBar({
           alignItems: "start",
           marginBottom: "1rem",
         }}>
-        <TooLTipButton
-          title={collapsed ? "expand" : "collapse"}
-          onClick={(e) =>
-            dispatchJson({
-              type: "updateJsonData",
-              payload: { index: dataIndex, data: { collapsed: !collapsed } },
-            })
-          }>
-          {collapsed ? (
-            <ExpandMore fontSize="large" />
-          ) : (
-            <ExpandLess fontSize="large" />
-          )}
-        </TooLTipButton>
+        {type === "Array" ? (
+          <TooLTipButton
+            title={collapsed ? "expand" : "collapse"}
+            onClick={(e) =>
+              dispatchJson({
+                type: "updateJsonData",
+                payload: { index: dataIndex, data: { collapsed: !collapsed } },
+              })
+            }>
+            {collapsed ? (
+              <ExpandMore fontSize="large" />
+            ) : (
+              <ExpandLess fontSize="large" />
+            )}
+          </TooLTipButton>
+        ) : (
+          <Box width="51px" height="51px"></Box>
+        )}
 
         <Box
           sx={(theme) => ({
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
+            gap: "0.5rem",
 
             [theme.breakpoints.only("xs")]: {
               gap: "1rem",
               flexDirection: "column",
+              alignItems: "flex-end",
             },
           })}>
+          {selectType === "default" && parentType !== "Array" && (
+            <TextField size="small" sx={{ width: "8rem" }} label="value" />
+          )}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            {parentType === "Array" && `${getArrayIndex()}:`}
+            <Select
+              label="type"
+              items={types}
+              value={type}
+              onChange={(e: any) => {
+                const { value } = e.target;
+
+                if (!previousType.current) {
+                  previousType.current = value;
+                  dispatchJson({
+                    type: "updateJsonData",
+                    payload: {
+                      index: dataIndex,
+                      data: { type: value },
+                    },
+                  });
+                  dispatchJson({
+                    type: "addField",
+                    payload: {
+                      data: {
+                        parentIndex: dataIndex,
+                        index: jsonData.data.length,
+                      },
+                    },
+                  });
+                } else if (previousType.current !== value) {
+                  previousType.current = value;
+                  dispatchJson({
+                    type: "replaceField",
+                    payload: {
+                      index: dataIndex,
+                      data: { type: value },
+                    },
+                  });
+                }
+              }}
+            />
+          </Box>
           {selectType === "default" && (
             <TextField size="small" sx={{ width: "8rem" }} label="field" />
           )}
-          <Select
-            label="type"
-            items={TOP_PARENT_TYPES}
-            value={type}
-            onChange={(e: any) => {
-              const { value } = e.target;
-              if (!previousType.current) {
-                previousType.current = value;
-                dispatchJson({
-                  type: "updateJsonData",
-                  payload: {
-                    index: dataIndex,
-                    data: { type: value },
-                  },
-                });
+          {isMultipleType() && (
+            <Button
+              onClick={() =>
                 dispatchJson({
                   type: "addField",
                   payload: {
@@ -92,30 +147,25 @@ export function SelectorBar({
                       index: jsonData.data.length,
                     },
                   },
-                });
-              } else if (previousType.current !== value) {
-                previousType.current = value;
-                dispatchJson({
-                  type: "replaceField",
-                  payload: {
-                    index: dataIndex,
-                    data: { type: value },
-                  },
-                });
-              }
-            }}
-          />
+                })
+              }>
+              add field
+            </Button>
+          )}
         </Box>
       </Box>
-      <Divider />
-      {!collapsed && (
-        <Box sx={{ padding: "1rem" }}>
-          {childrenIndexes.length
-            ? getChildren().map((data) => {
-                return <SelectorBar dataIndex={data.index} />;
-              })
-            : "select type"}
-        </Box>
+
+      {!collapsed && isMultipleType() && (
+        <>
+          <Divider />
+          <Box sx={{ padding: "1rem" }}>
+            {childrenIndexes.length
+              ? getChildren().map((data) => {
+                  return <SelectorBar dataIndex={data.index} />;
+                })
+              : "select type"}
+          </Box>
+        </>
       )}
     </Box>
   );
--- src/data/typesData.ts
+++ src/data/typesData.ts
@@ -1,1 +1,10 @@
 export const TOP_PARENT_TYPES = ["Array", "Object"];
+export const TYPES = [
+  "String",
+  "Number",
+  "Boolean",
+  "Array",
+  "Object",
+  "Null",
+  "Undefined",
+];
